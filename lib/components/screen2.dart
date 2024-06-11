8879c171d4244dc3e1d742879a3aeae93cd8d8cf
choihojean <chlghwls1231@naver.com>
2024-06-11 11:05:59 +0900

    Update screen2.dart

--- lib/components/screen2.dart
+++ lib/components/screen2.dart
@@ -10,19 +10,43 @@ class Screen2 extends StatelessWidget {
           _buildListItem(
             context,
             image: 'assets/pullup.png',
-            title: 'Item 1',
-            subtitle: 'Test subtitle',
+            title: '풀 업',
+            subtitle: '풀업(턱걸이)',
           ),
           _buildListItem(
             context,
             image: 'assets/latpulldown.png',
-            title: 'Item 2',
+            title: '렛 풀다운',
+            subtitle: '등',
+          ),
+          _buildListItem(
+            context,
+            image: 'assets/seatedrow.png',
+            title: '시티드 케이블 로우',
+            subtitle: '등',
+          ),
+          _buildListItem(
+            context,
+            image: 'assets/seatedrow.png',
+            title: 'Item 4',
             subtitle: 'Test subtitle',
           ),
           _buildListItem(
             context,
             image: 'assets/seatedrow.png',
-            title: 'Item 3',
+            title: 'Item 5',
+            subtitle: 'Test subtitle',
+          ),
+          _buildListItem(
+            context,
+            image: 'assets/seatedrow.png',
+            title: 'Item 6',
+            subtitle: 'Test subtitle',
+          ),
+          _buildListItem(
+            context,
+            image: 'assets/seatedrow.png',
+            title: 'Item 7',
             subtitle: 'Test subtitle',
           ),
           // 추가적인 리스트 아이템들을 필요에 따라 추가할 수 있습니다.
